37eaa24710325a28a508efd13f703c7135df2e6a
Praful Shrestha <prafulshrestha.softace@outlook.com>
2023-03-10 22:45:16 +0545

    .

--- Lab 2/Server side scripting/Q2/dashboard.php
+++ Lab 2/Server side scripting/Q2/dashboard.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// if(!isset($_SESSION['email'])) {
-//     header("Location: login.php");
-//     exit;
-// }
+if(!isset($_SESSION['email'])) {
+    header("Location: login.php");
+    exit;
+}
 
 if(isset($_POST['logout'])) {
     session_unset(); // clears all session variables
